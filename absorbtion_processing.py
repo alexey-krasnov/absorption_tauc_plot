ccd51bc68b34f7924199aa4da097e0e4ec4e5875
Aleksei Krasnov <alexey-krasnov@rambler.ru>
2021-11-30 16:10:13 +0300

    Pre_alpha abs project

--- absorbtion_processing.py
+++ absorbtion_processing.py
@@ -56,11 +56,14 @@ def indirect_plot():
     ax.plot(df['Energy, eV'], df['Indirect transition'])
     plt.savefig(i.replace('.txt', '_indirect_Tauc.png'), dpi=300)
 
+# Check if you have already run the program for the files
+# check_file = os.path.exists('*+.txt')
+# print(check_file)
+if not glob.glob('*+.txt'):
 
 # Read initial csv files from the current directory
-for i in glob.glob('*.txt'):
-    # Check if you have already run the program for the files
-    if i not in glob.glob('*+.txt'):
+    for i in glob.glob('*.txt'):
+      # if i.replace('.txt', '+.txt') not in glob.glob('*+.txt'):
         df = pd.read_csv(i, sep=",")
         # Calculation of the corresponding energy and Tauc transformation for direct/indirect allowed transition
         df['Energy, eV'] = 1240 / df['Wavelength (nm)']
@@ -80,8 +83,7 @@ for i in glob.glob('*.txt'):
         elif int(n) == 1:
             direct_plot()
             indirect_plot()
-    else:
-        print("You have already generated necessary files")
-        continue
+else:
+    print("You have already generated necessary files")
 
 print("Processing of your absorption data is finished!")
