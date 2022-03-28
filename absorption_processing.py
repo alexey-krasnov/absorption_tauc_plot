44956eb8bfea5cc040cfed19a8e10ac9f729130f
Aleksei Krasnov <alexeykrasnov1989@gmail.com>
2022-03-28 16:53:23 +0300

    Refractoring

--- absorption_processing.py
+++ absorption_processing.py
@@ -41,16 +41,13 @@ def ask_semiconductor_type(file_name: str) -> float:
     return n
 
 
-
-
-
 def data_processing(df, n):
     """Read data from csv files in the current directory, making Tauc transformation, writing, and exporting data.
     Calculation of the corresponding energy values and Tauc transformation for direct/indirect allowed transition."""
-    df['Energy, eV'] = 1240 / df['Wavelength (nm)']
-    df['Direct transition'] = (df['Absorbance'] * df['Energy, eV']) ** 2
+    df['Energy (eV)'] = 1240 / df['Wavelength (nm)']
+    df['Direct transition'] = (df['Absorbance'] * df['Energy (eV)']) ** 2
     if n == 0.5:
-        df['Indirect transition'] = (df['Absorbance'] * df['Energy, eV']) ** n
+        df['Indirect transition'] = (df['Absorbance'] * df['Energy (eV)']) ** n
     # Export excel and/or txt files, comment-uncomment if necessary.
     # df.to_excel(i.replace('txt', 'xlsx'), 'Sheet1', index=False)
     df.to_csv(file.replace('.txt', '_out.txt'), sep=',', index=False)
@@ -74,7 +71,7 @@ def absorption_plot(df, file_name):
     plt.savefig(file_name.replace('txt', 'png'), dpi=300)
 
 
-def tauc_gen(df, n):
+def tauc_generator(df, n):
     """Generator of pandas Series from processed Data Frame
     depending on type of semiconductor: direct or indirect """
     if n == 0.5:
@@ -85,7 +82,8 @@ def tauc_gen(df, n):
 
 
 def linearization(x_axis, y_axis):
-    #  Convert Pandas Series 'Energy, eV'; 'Direct transition'/'Indirect transition' to NumPy arrays
+    """Make linearization of 'Direct transition'/'Indirect transition' plot vs energy"""
+    #  Convert Pandas Series 'Energy (eV)'; 'Direct transition'/'Indirect transition' to NumPy arrays
     x_numpy = x_axis.to_numpy()
     y_numpy = y_axis.to_numpy()
     # Smooth y line
@@ -95,18 +93,18 @@ def linearization(x_axis, y_axis):
     dy = np.diff(y_smooth, 1)
     # Select the global maximum point on the graph, smooth dy/dx plot
     max_index = np.argmax(savgol_filter(dy / dx, 101, 3))
-
     x_linear = x_numpy[max_index - 10: max_index + 10]
     y_linear = y_smooth[max_index - 10: max_index + 10]
     return x_linear, y_linear, max_index
 
 
-def calc_linear_coeff(x_linear, y_linear):
+def calc_linear_coeff(x_linear, y_linear) -> tuple:
+    """Calculate a regression line and return its coefficients"""
     a, b, r_value, p_value, stderr = linregress(x_linear, y_linear)
     return a, b, r_value, p_value, stderr
 
 
-def calc_band_gap(a, b, y_axis):
+def calc_band_gap(a, b, y_axis) -> float:
     """Calculate direct/indirect band gap value"""
     e_band_gap = round(-b / a, 2)
     print(f"{y_axis.name} band gap is: {e_band_gap}")
@@ -114,11 +112,13 @@ def calc_band_gap(a, b, y_axis):
 
 
 def vizual_x(e_g, x_axis, max_index):
+    """Return region of abscissa to plot a regression line"""
     visualization_x = np.linspace(e_g, x_axis[max_index - 60], 2)
     return visualization_x
 
 
 def func(visualization_x, a, b):
+    """Define and return a regression line function as 'y = a*x + b' """
     return a*visualization_x + b
 
 
@@ -138,7 +138,14 @@ def tauc_plot(x_axis, y_axis, file_name, e_g, a, b, visualization_x):
     ax.plot(x_axis, y_axis)
     ax.scatter(e_g, 0, marker='x', color='k', label=f"Band gap {e_g} eV")
     ax.plot(visualization_x, func(visualization_x, a, b), color='red')
+    ax.legend()
     plt.savefig(file_name.replace('.txt', f'_{y_axis.name}.png'), dpi=300)
+
+
+# def save_database(file_name: str, e_g: float):
+#     """Save Eg values in the csv file"""
+
+
 
 
 if __name__ == "__main__":
@@ -160,12 +167,12 @@ if __name__ == "__main__":
         # Plot figures of the absorption spectra and Tauc transformation
         absorption_plot(df=processed_df, file_name=file)
         # Work with 'Direct/Indirect transition' series from DataFrame
-        tauc_series = tauc_gen(df=processed_df, n=tauc_indicator)
+        tauc_series = tauc_generator(df=processed_df, n=tauc_indicator)
         for tauc in tauc_series:
-            x_linear, y_linear, max_index = linearization(x_axis=processed_df['Energy, eV'], y_axis=tauc)
+            x_linear, y_linear, max_index = linearization(x_axis=processed_df['Energy (eV)'], y_axis=tauc)
             a, b, r_value, p_value, stderr = calc_linear_coeff(x_linear, y_linear)
             e_g = calc_band_gap(a, b, y_axis=tauc)
-            visualization_x = vizual_x(e_g, x_axis=processed_df['Energy, eV'], max_index=max_index)
-            tauc_plot(processed_df['Energy, eV'], tauc, file, e_g, a, b,visualization_x)
+            visualization_x = vizual_x(e_g, x_axis=processed_df['Energy (eV)'], max_index=max_index)
+            tauc_plot(processed_df['Energy (eV)'], tauc, file, e_g, a, b, visualization_x)
     else:
         print("Processing of your absorption data is finished successfully!")
